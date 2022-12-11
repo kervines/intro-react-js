69b3cd6bced0191fd8b72caeed9577c44b85e8da
kervines <kervinzero@gmail.com>
2022-12-11 10:46:50 -0300

    Adicionado componentes de botoes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+
+import { Button } from "../../components/Button";
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <Button title="teste 1" />
+      <Button variant="secondary" title="teste 2" />
       <Link to="/login">Fazer login</Link>
     </>
   );
