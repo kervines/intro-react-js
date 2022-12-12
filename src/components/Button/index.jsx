7fb1174213d4ff84abd93dc83bb6d49b5b8f5bcd
kervines <kervinzero@gmail.com>
2022-12-11 21:45:56 -0300

    adição dos componentes de estilização

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,7 +5,7 @@ import { ButtonContainer } from "./style";
 const Button = ({ title, variant = "primary", onClick }) => {
   return (
     <ButtonContainer variant={variant} onClick={onclick}>
-      {title};
+      {title}
     </ButtonContainer>
   );
 };
--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -18,15 +18,20 @@ export const ButtonContainer = styled.button`
 
       background: #e4105d;
 
+      &:hover {
+        opacity: 0.6;
+        cursor: pointer;
+      }
+
       &::after {
-        content: "",
+        content: "";
         position: absolute;
         border: 1px solid #e4105d;
         top: -5px;
         left: -6px;
-        width: calc(100% - 10px);
-        heigth: calc(100% - 10px);
+        width: calc(100% + 10px);
+        height: calc(100% + 10px);
         border-radius: 22px;
-  }
-  `}
+      }
+    `}
 `;
